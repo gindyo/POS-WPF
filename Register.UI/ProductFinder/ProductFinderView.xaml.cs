0f824a57985f6209614cf2bc9dcf1b0ce70eed0e
dimitar <gindio@gmail.com>
2015-03-24 08:58:43 -0500

    Renamed Product Search -> Product Finder

---
+++ Register.UI/ProductFinder/ProductFinderView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Register.UI.ProductFinder
+{
+    /// <summary>
+    ///     Interaction logic for ProductFinderView.xaml
+    /// </summary>
+    public partial class ProductFinderView
+    {
+        public ProductFinderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
